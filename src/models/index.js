1c6af9af33ba5e25dc63e538188ffa482e8c5282
ivansteyer <118127891+ivansteyer@users.noreply.github.com>
2025-08-31 14:59:04 +0200

    Fix tenant property mapping

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require("sequelize");
 const path = require("path");
 require("dotenv").config();
+// Resolve to project-root/config/dev.sqlite by default
 const url =
   process.env.DATABASE_URL ||
-  "sqlite:" + path.join(__dirname, "../config/dev.sqlite");
+  "sqlite:" + path.resolve(__dirname, "../../config/dev.sqlite");
 const sequelize = new Sequelize(url, { logging: false });
 module.exports = sequelize;
  
